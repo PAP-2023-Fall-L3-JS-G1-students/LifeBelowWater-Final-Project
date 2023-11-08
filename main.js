852cc098bcd01f34b92cc9225534f69b5743a2ac
Cynthia Chand <137232715+CynTheUhh@users.noreply.github.com>
2023-11-07 19:10:48 -0800

    Added form content

---
+++ main.js
@@ -0,0 +1,9 @@
+/*let form = document.querySelector("#form");
+
+function generate(event) {
+    event.preventDefault();
+
+    let hours = document.querySelector("#hours").value;
+}
+
+form.addEventListener("submit", generate);
