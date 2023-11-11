a15432c3eab4ac352fccf648e0e2c9f3c3308cd8
Cynthia Chand <137232715+CynTheUhh@users.noreply.github.com>
2023-11-10 22:44:14 -0800

    Centered form outputs

--- main.js
+++ main.js
@@ -10,16 +10,16 @@ const container = document.querySelector("#form-background");
 console.log(container);
 
     if (hours < 2) {
-        container.innerText = "Educate yourself on ways you can reduce your carbon emissions and conserve water!";
+        container.innerHTML = "<br><br><br><br>Educate yourself on ways you can reduce your carbon emissions and conserve water!";
     }
         else if (hours >= 2 && hours < 4) {
-        container.innerText = "Spread the word and tell your friends and family about what they can do to help too!";
+        container.innerHTML = "<br><br><br><br>Spread the word and tell your friends and family about what they can do to help too!";
     }
         else if (hours >= 4 && hours < 6) {
-        container.innerText = "Reach out to your local representatives!";
+        container.innerHTML = "<br><br><br><br><br>Reach out to your local representatives!";
     }
         else {
-        container.innerText = "Volunteer for a debris cleanup program!, Link to Puget Soundkeeper: https://pugetsoundkeeper.org/volunteer/ ";
+        container.innerHTML = "<br><br><br>Volunteer for a debris cleanup program! <br><br> Link to Puget Soundkeeper: https://pugetsoundkeeper.org/volunteer/ ";
         }
 }
 
