b246248d55f53a93c1a670ad1e6434fc02404d6b
Cynthia Chand <137232715+CynTheUhh@users.noreply.github.com>
2023-11-11 14:56:48 -0800

    Updated cursor images, fixed css in about page, added video embed to home

--- main.js
+++ main.js
@@ -9,17 +9,23 @@ function generate(event) {
 const container = document.querySelector("#form-background");
 console.log(container);
 
-    if (hours < 2) {
-        container.innerHTML = "<br><br><br><br>Educate yourself on ways you can reduce your carbon emissions and conserve water!";
+    if (hours < 0) {
+        container.innerHTML = "<div>Hey, that's not how time works 😠</div>";
+    } 
+        else if (hours < 2) {
+        container.innerHTML = "<div>Educate yourself on ways you can reduce your carbon emissions and conserve water!</div>";
     }
         else if (hours >= 2 && hours < 4) {
-        container.innerHTML = "<br><br><br><br>Spread the word and tell your friends and family about what they can do to help too!";
+        container.innerHTML = "<div>Spread the word and tell your friends and family about what they can do to help too!</div>";
     }
         else if (hours >= 4 && hours < 6) {
-        container.innerHTML = "<br><br><br><br><br>Reach out to your local representatives!";
+        container.innerHTML = "<div>Reach out to your local representatives!</div>";
+    }
+        else if (hours >= 6 && hours < 720) {
+        container.innerHTML = "<div>Volunteer for a debris cleanup program! <br><br> Link to Puget Soundkeeper: https://pugetsoundkeeper.org/volunteer/</div>";
     }
         else {
-        container.innerHTML = "<br><br><br>Volunteer for a debris cleanup program! <br><br> Link to Puget Soundkeeper: https://pugetsoundkeeper.org/volunteer/ ";
+        container.innerHTML = "<div>Hey, that's more hours than there are in a month, calm down 😠</div>";
         }
 }
 
