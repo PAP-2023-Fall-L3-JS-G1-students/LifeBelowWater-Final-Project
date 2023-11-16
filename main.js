21c0ff00ccb8e0476a3781dd576b1a9579729845
Samantha <sambamtreacy@gmail.com>
2023-11-16 15:17:35 -0800

    made final changes

--- main.js
+++ main.js
@@ -13,16 +13,17 @@ console.log(container);
         container.innerHTML = "<div>Hey, that's not how time works 😠</div></br>";
     } 
         else if (hours < 2) {
-        container.innerHTML = "<div>Educate yourself on ways you can reduce your carbon emissions and conserve water!</div>";
+        container.innerHTML = "<div>Educate yourself on ways you can reduce your carbon emissions and conserve water! <br><br> https://www.austintexas.gov/blog/top-10-ways-reduce-your-carbon-footprint-and-save-money</div>";
     }
         else if (hours >= 2 && hours < 4) {
         container.innerHTML = "<div>Spread the word and tell your friends and family about what they can do to help too!</div>";
     }
-        else if (hours >= 4 && hours < 6) {
-        container.innerHTML = "<div>Reach out to your local representatives!</div>";
+        else if (hours >= 4 && hours < 8) {
+        container.innerHTML = "<div>Reach out to your local representatives! <br><br> https://leg.wa.gov/House/Representatives/Pages/default.aspx</div>";
     }
-        else if (hours >= 6 && hours < 720) {
+        else if (hours >= 8 && hours < 720) {
         container.innerHTML = "<div>Volunteer for a debris cleanup program! <br><br> Link to Puget Soundkeeper: https://pugetsoundkeeper.org/volunteer/</div>";
+        
     }
         else {
         container.innerHTML = "<div>Hey, that's more hours than there are in a month, calm down 😠</div>";
